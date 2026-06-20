5fc2d27585d8cb5768dd05a2df8cb85d7a41d55a
Samuel Baquero <samsantbaq@gmail.com>
2026-06-20 14:05:16 -0500

    debug try

--- components/Header.tsx
+++ components/Header.tsx
@@ -54,7 +54,7 @@ const Header = () => {
         </div>
         {/* Right compartment */}
         <div className={clsx(
-            "h-fit bb lg:py-2",
+            "bb lg:py-2",
             "flex justify-center items-center",
             "rounded-md lg:rounded-xl border",
             "duration-300",
