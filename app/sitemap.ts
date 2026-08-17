675dbc5ec6cc6f6d855ed8f7489776e99b935d61
Samuel Baquero <samsantbaq@gmail.com>
2026-08-17 04:40:40 -0500

    sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,22 @@
+import { MetadataRoute } from 'next'
+import company from '@/data/company'
+
+const BASE_URL = company.url
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const frequency: MetadataRoute.Sitemap[number]['changeFrequency'] = 'weekly'
+
+    const routes = [
+        '',
+        '/portafolio',
+        '/quien-soy',
+        '/cotizaciones'
+    ].map(route => ({
+        url: `${BASE_URL}${route}`,
+        lastModified: new Date(),
+        changeFrequency: frequency,
+        priority: route === "" ? 1 : 0.8,
+    }))
+
+    return routes
+}
