c7b0bc5317470af79b60328774429953058dcbc9
Samuel Baquero <samsantbaq@gmail.com>
2026-08-17 23:06:36 -0500

    another fix

--- lib/contentful-queries.ts
+++ lib/contentful-queries.ts
@@ -27,7 +27,6 @@ export async function getMedia(
 export async function getFeaturedImages(): Promise<Media[]> {
   const entries = await contentful.getEntries<MediaSkeleton>({
     content_type: 'media',
-    "fields.featured": true,
     'fields.type': 'Fotografía'
   })
 
