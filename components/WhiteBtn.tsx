1ac7f39ce21a8130b58f68be5db7703b30dd4c43
Samuel Baquero <samsantbaq@gmail.com>
2026-08-25 18:45:37 -0500

    bigger button

--- components/WhiteBtn.tsx
+++ components/WhiteBtn.tsx
@@ -17,7 +17,7 @@ const WhiteBtn = ({ href, label, centered = true }: Props) => {
             "px-6 py-3",
             "lg:px-8 lg:py-4",
             "bg-brandwhite",
-            "text-black text-my-md font-semibold",
+            "text-black text-myf-lg font-semibold",
             "rounded-full",
             "md:hover:scale-110 duration-400 ease-out"
         )}
